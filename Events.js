8279e3cff400a6bf46c3cd2d5d15afee6829b482
Peyton-lee <771412508@qq.com>
2018-01-02 09:57:12 +0800

    first commit

---
+++ Events.js
@@ -0,0 +1,119 @@
+/**
+ * Created by Peyton on 2017/12/29.
+ */
+;(function (exports) {
+
+    /*
+     sg.addEventListener(this);
+     this.eventEmitter.on("event", function (a, b) {
+     cc.log(a, b);
+     });
+     this.eventEmitter.emit("event", 1, 2);
+     this.eventEmitter.once("event", 1, 2);
+     */
+
+    if (exports.addEventListener || exports.removeEventListener) {
+        EventListener.prototype.error("addEventListener has been defined.");
+        return;
+    }
+
+    // @param: {scope | binding context "this"}
+    Object.defineProperty(exports, 'addEventListener', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function (scope) {
+            if (!!scope.eventEmitter) {
+                EventListener.prototype.error("The event has been added after. Do not need to add again.");
+                return;
+            }
+            scope.eventEmitter = new EventListener(scope);
+        }
+    });
+
+    Object.defineProperty(exports, 'removeEventListener', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function (scope) {
+            if (!scope.eventEmitter) {
+                EventListener.prototype.error("The event has been removed.");
+                return;
+            }
+            scope.eventEmitter = null;
+        }
+    });
+
+    // event class [EventListener]
+    function EventListener(scope) {
+        this._target = scope;
+        this._eventCaches = {};
+    };
+
+    // prototype
+    EventListener.prototype = {
+        constructor: EventListener,
+
+        error: function (logText) {
+            cc.log("[EVENTS ERROR]: " + logText);
+        },
+
+        getEventNames: function () {
+            var names = [];
+            for (var key in this._eventCaches) {
+                names.push(key);
+            }
+            return names;
+        },
+
+        on: function (eventName, callback) {
+            if (!eventName || typeof eventName !== "string") {
+                this.error("check your first parameter.");
+                return;
+            }
+            if (typeof callback !== "function") {
+                this.error("The callback is not a function.");
+                return;
+            }
+            if (this._eventCaches[eventName]) {
+                this.error("The event [" + eventName + "] has already existed.");
+                return;
+            }
+            this._eventCaches[eventName] = callback;
+        },
+
+        emit: function () {
+            var eventName = arguments[0];
+            if (!eventName || typeof eventName !== "string") {
+                this.error("Check your incoming parameters.");
+                return;
+            }
+            if (!this._eventCaches[eventName]) {
+                this.error("The event [" + eventName + "] has not existed.");
+                return;
+            }
+            // remove the first parameter
+            var params = Array.prototype.slice.call(arguments, 1, arguments.length);
+            this._eventCaches[eventName].apply(this._target, params);
+        },
+
+        once: function () {
+            this.emit.apply(this, arguments);
+            var eventName = arguments[0];
+            this._eventCaches[eventName] = null;
+        },
+
+        remove: function (eventName) {
+            if (!eventName || typeof eventName !== "string") {
+                this.error("The event [" + eventName + "] is not string.");
+                return;
+            }
+            if (!this._eventCaches[eventName]) {
+                this.error("The event [" + eventName + "] has been removed.");
+                return;
+            }
+            this._eventCaches[eventName] = null;
+        }
+    }
+
+})(sg || {});
